82892c570a3cfe68f12c2ff7006221a286cae03c
sjt88 <simonjtweed@googlemail.com>
2016-05-08 01:49:36 +0100

    asdasd

--- gulpfile.js
+++ gulpfile.js
@@ -102,8 +102,7 @@ gulp.task('babel', function() {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
-      presets: ['es2015'],
-      plugins: ['transform-runtime']
+      presets: ['es2015']
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
