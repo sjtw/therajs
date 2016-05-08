8abd40e85e8770a73992adb16f920f75372d5d76
sjt88 <simonjtweed@googlemail.com>
2016-05-08 03:14:13 +0100

    build 1.0.4

--- dist/thera.js
+++ dist/thera.js
@@ -1,13 +1,2 @@
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.XmlClient = undefined;
-
-var _XmlClient = require('./clients/XmlClient');
-
-var _XmlClient2 = _interopRequireDefault(_XmlClient);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.XmlClient = _XmlClient2.default;
+Object.defineProperty(exports,"__esModule",{value:true});exports.XmlClient=undefined;var _XmlClient=require('./clients/XmlClient');var _XmlClient2=_interopRequireDefault(_XmlClient);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.XmlClient=_XmlClient2.default;
 //# sourceMappingURL=thera.js.map
