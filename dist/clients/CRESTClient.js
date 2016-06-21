7027a0c59b6163da595434f189e47f7d69f9a3f3
sjt88 <simonjtweed@googlemail.com>
2016-06-21 15:07:08 +0100

    build v1.0.6

---
+++ dist/clients/CRESTClient.js
@@ -0,0 +1,3 @@
+function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}var CRESTClient=function CRESTClient(args){ // code
+_classCallCheck(this,CRESTClient);};
+//# sourceMappingURL=CRESTClient.js.map
